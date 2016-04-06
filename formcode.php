bf7b6330003b6db72f73e0123f42bc8d7cc9c6d7
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-06 20:11:24 +0300

    Update formcode.php

--- formcode.php
+++ formcode.php
@@ -1,15 +1,16 @@
-
+<div style='text-align:center;background-color:black;'>
 <form method='post'>
-  Name: <input type='text' name='name' id='name' /><br />
+  Name: <input type='text' name='name' id='name'  /><br />
 
   Email:  <input type='text' name='email' id='email' /><br />
 
   Website: <input type='text' name='website' id='website' /><br />
 
-  Comment:<br />
+Comment:
   <textarea name='comment' id='comment'></textarea><br />
 
   <input type='hidden' name='articleid' id='articleid' value='<?php echo $_GET["id"]; ?>' />
 
   <input type='submit' value='Submit' />  
 </form>
+</div>
