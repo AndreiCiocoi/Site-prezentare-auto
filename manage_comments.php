e8b7f868f594d8e4f4003d11e75b02ac0887b604
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-10 19:57:12 +0300

    Update manage_comments.php

--- manage_comments.php
+++ manage_comments.php
@@ -11,22 +11,20 @@ if( $_POST )
   mysql_select_db('comm', $con);
 
   $users_name = $_POST['name'];
-  $users_email = $_POST['email'];
-  $users_website = $_POST['website'];
+  
   $users_comment = $_POST['comment'];
 
   $users_name = mysql_real_escape_string($users_name);
-  $users_email = mysql_real_escape_string($users_email);
-  $users_website = mysql_real_escape_string($users_website);
+ 
   $users_comment = mysql_real_escape_string($users_comment);
 
   $articleid = $_GET['id'];
   if( ! is_numeric($articleid) )
     die('invalid article id');
 
-  $query = "INSERT INTO `comments` (`id`, `name`, `email`, `website`,
+  $query = "INSERT INTO `comments` (`id`, `name`,
         `comment`, `timestamp`, `articleid`) VALUES (NULL, '$users_name',
-        '$users_email', '$users_website', '$users_comment',
+        '$users_comment',
         CURRENT_TIMESTAMP, '$articleid');";
 
   mysql_query($query);
