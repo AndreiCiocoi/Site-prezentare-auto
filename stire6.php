b2ed630956cffb9391184c3650f24c83db25afee
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-20 13:24:54 +0300

    Update stire6.php

--- stire6.php
+++ stire6.php
@@ -38,15 +38,20 @@
  			centimetri. Cursa (Stroke in engleza) este distanta pe care o face pistonul din pozitia cea mai joasa
  			in pozitia cea mai inalta, calculata in centimetri.</p>
  
- <p style='font-size:120%;text-align:left;'>Volumul unui piston se calculeaza:<b> Pi/4 X alezaj la puterea a doua X cursa</b></p>
- <p style='font-size:120%;'>De exemplu, sa spunem ca un cilindru are un alezaj (diametru) de 10 centimetri
- si o cursa de 4 centimetri. Calculand dupa formula urmatoare ne iese capacitatea cilindrica:</p>
- <p style='font-size:120%;text-align:left;'>3.1416/4 × (10 cm)2 × 5 cm × 4 = 1,570 cm3 = 1.57 litri</p>
- <a target="_blank" href="imagini/capacitcil2.jpg"><img src="imagini/capacitcil2.jpg"></a>
- <p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
-</article>
+ 			<p style='font-size:120%;text-align:left;'>Volumul unui piston se calculeaza:<b> Pi/4 X alezaj la puterea a doua X cursa</b></p>
+ 
+ 			<p style='font-size:120%;'>De exemplu, sa spunem ca un cilindru are un alezaj (diametru) de 10 centimetri
+ 			si o cursa de 4 centimetri. Calculand dupa formula urmatoare ne iese capacitatea cilindrica:</p>
+ 
+ 			<p style='font-size:120%;text-align:left;'>3.1416/4 × (10 cm)2 × 5 cm × 4 = 1,570 cm3 = 1.57 litri</p>
+ 	
+ 			<a target="_blank" href="imagini/capacitcil2.jpg"><img src="imagini/capacitcil2.jpg"></a>
+ 
+ 			<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+		</article>
+	</div>
 </div>
-</div>
+
 <?php
 include("manage_comments.php");
 include("display_comments.php");
