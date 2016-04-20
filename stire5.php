45f30dbf16dcb3aeb7f4f63fa9c70af635054ee9
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-20 13:18:21 +0300

    Update stire5.php

--- stire5.php
+++ stire5.php
@@ -39,37 +39,45 @@
   				stabilizeaza curentul electric (ballast), sistem automat de reglaj in functie de sarcina, spalator far.</span></p>
  
  				<p style='font-size:120%;'><b>Cu alte cuvinte, daca tu ai pe masina faruri cu xenon, facute de tine
- prin simpla montare a unor becuri, sau cine stie ce chinezarii ordinare, fara sa ai toate cele 
- mentionate mai sus, risti sa iti pierzi talonul si sa fii chemat la RAR.</b></p>
- <p style='font-size:120%;'><b>Cine doreste sa fie in legalitate are doar doua variante. Prima este sa-si 
- cumpere sistemul de iluminare original, special pentru un anume model de masina, sistem care a fost
- montat pe vehicule similare,</b> dar care sa cuprinda obligatoriu faruri specifice, surse de lumina cu 
- descarcare in gaz, dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza
- curentul electric (ballast), sistem automat de reglaj in functie de sarcina, spalator far. 
- Adica sa aiba far cu lupa, becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri. 
- <b>Din fericire, toate farurile OEM cu xenon au asa ceva.</b></p>
- <a target="_blank" href="imagini/faruribmw.jpg"><img src="imagini/faruribmw.jpg"></a>
- <p id="border" style='color:white;font-size:120%;'>Faruri originale de BMW cu xenon din fabrica</p>
- <p style='font-size:120%;'><b>Montarea farurilor cu xenon OEM, adica originale, fie ele noi, fie de la 
- dezmembrari, trebuie sa se faca exclusiv intr-un centru autorizat al reprezentantei constructorului 
- in Romania. </b>Adica daca vrei sa-ti pui xenon pe un Seria 3 E46, trebuie sa mergi la Automobile Bavaria.
- Acestia trebuie sa-ti dea o adresa in care sunt enumerate elementele montate si se mentioneaza ca 
- sunt originale, de prim montaj.</p>
-  <p style='font-size:120%;'>Cu aceasta adresa de la service, esti obligat sa te duci la Registrul Auto
-  Roman pentru a le omologa si pentru a le trece in cartea de identitate. Asta costa circa 150 de lei.</p>
-   <p style='font-size:120%;'><b>O alta varianta este montarea unor faruri aftermarket.</b> Acestea trebuie 
-   neaparat sa fie montate cu kitul obligatoriu: faruri specifice, surse de lumina cu descarcare in gaz,
-   dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza curentul electric 
-   (ballast), sistem automat de reglaj in functie de sarcina, spalator far. Adica sa aiba far cu lupa,
-   becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri.</p>
-   <p style='font-size:120%;'>Montarea farurilor cu xenon aftermarket, numite oficial retrofit, 
-   este obligatoriu sa fie realizata intr-un service omologat pentru modificari constructive. 
-   Acest service trebuie sa-ti ofere la finalul montajului o declaratie de conformitate emisa de 
-   inginerii de acolo, in care sunt enumerate elementele montate, cu specificarea numerelor de omologare
-   CE/ECE-ONU pentru fiecare element in parte.</p>
-    <p style='font-size:120%;'><b>Ulterior, cei care si-au pus pe masina faruri cu xenon aftermarket, 
-	trebuie sa mearga la o reprezentanta RAR pentru a mentionarea in carte: “Echipare retrofit cu surse 
-	de lumină cu descărcare în gaz”. Asta inseamna modificare in carte care costa circa 150 de lei.</b></p>
+ 				prin simpla montare a unor becuri, sau cine stie ce chinezarii ordinare, fara sa ai toate cele 
+ 				mentionate mai sus, risti sa iti pierzi talonul si sa fii chemat la RAR.</b></p>
+ 
+ 				<p style='font-size:120%;'><b>Cine doreste sa fie in legalitate are doar doua variante. Prima este sa-si 
+ 				cumpere sistemul de iluminare original, special pentru un anume model de masina, sistem care a fost
+ 				montat pe vehicule similare,</b> dar care sa cuprinda obligatoriu faruri specifice, surse de lumina cu 
+ 				descarcare in gaz, dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza
+ 				curentul electric (ballast), sistem automat de reglaj in functie de sarcina, spalator far. 
+ 				Adica sa aiba far cu lupa, becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri. 
+ 				<b>Din fericire, toate farurile OEM cu xenon au asa ceva.</b></p>
+ 
+ 				<a target="_blank" href="imagini/faruribmw.jpg"><img src="imagini/faruribmw.jpg"></a>
+ 
+ 				<p id="border" style='color:white;font-size:120%;'>Faruri originale de BMW cu xenon din fabrica</p>
+ 
+ 				<p style='font-size:120%;'><b>Montarea farurilor cu xenon OEM, adica originale, fie ele noi, fie de la 
+ 				dezmembrari, trebuie sa se faca exclusiv intr-un centru autorizat al reprezentantei constructorului 
+ 				in Romania. </b>Adica daca vrei sa-ti pui xenon pe un Seria 3 E46, trebuie sa mergi la Automobile Bavaria.
+ 				Acestia trebuie sa-ti dea o adresa in care sunt enumerate elementele montate si se mentioneaza ca 
+ 				sunt originale, de prim montaj.</p>
+  
+  				<p style='font-size:120%;'>Cu aceasta adresa de la service, esti obligat sa te duci la Registrul Auto
+  				Roman pentru a le omologa si pentru a le trece in cartea de identitate. Asta costa circa 150 de lei.</p>
+   
+   				<p style='font-size:120%;'><b>O alta varianta este montarea unor faruri aftermarket.</b> Acestea trebuie 
+   				neaparat sa fie montate cu kitul obligatoriu: faruri specifice, surse de lumina cu descarcare in gaz,
+   				dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza curentul electric 
+   				(ballast), sistem automat de reglaj in functie de sarcina, spalator far. Adica sa aiba far cu lupa,
+   				becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri.</p>
+   
+   				<p style='font-size:120%;'>Montarea farurilor cu xenon aftermarket, numite oficial retrofit, 
+   				este obligatoriu sa fie realizata intr-un service omologat pentru modificari constructive. 
+   				Acest service trebuie sa-ti ofere la finalul montajului o declaratie de conformitate emisa de 
+   				inginerii de acolo, in care sunt enumerate elementele montate, cu specificarea numerelor de omologare
+   				CE/ECE-ONU pentru fiecare element in parte.</p>
+    
+    				<p style='font-size:120%;'><b>Ulterior, cei care si-au pus pe masina faruri cu xenon aftermarket, 
+				trebuie sa mearga la o reprezentanta RAR pentru a mentionarea in carte: “Echipare retrofit cu surse 
+				de lumină cu descărcare în gaz”. Asta inseamna modificare in carte care costa circa 150 de lei.</b></p>
  <p style='font-size:120%;'>Cine este prins de autoritati ca circula cu o masina care are faruri cu xenon
  ce nu respecta aceste reguli, ramane fara talon si primeste o sanctiune de 1-2 puncte amenda, adica 
  intre 105 si 210 lei. Mai mult, cine ajunge la RAR pentru alte operatiuni, cum ar fi identificarea in
