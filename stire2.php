00be5e4e8cf54a30cc66b01e6686403b38144900
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-20 12:03:31 +0300

    Update stire2.php

--- stire2.php
+++ stire2.php
@@ -1,68 +1,89 @@
 <div  style='background-color:black;'>
- <div id="header" style='width: 300px;height: 10px;margin: 0px auto;'>
-<div id="logo"style='float:left;'>	
-	<a href="index.html" style='color:#999999;text-decoration: none;'><h1>BestCars</h1></a>
+	
+	<div id="header" style='width: 300px;height: 10px;margin: 0px auto;'>
+
+		<div id="logo"style='float:left;'>	
+			<a href="index.html" style='color:#999999;text-decoration: none;'><h1>BestCars</h1></a>
+		</div>
+	
+		<div id="logocar" style='float:right;'>	
+			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
+		</div>
 	</div>
-	<div id="logocar" style='float:right;'>	
-	<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
+	
+	<div style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
+		<img src="imagini/masinalovita.jpg">
+		<article>
+			<h1 style='color:#0066cc;'>Cum sa recunoastem o masina lovita si reparata in doar 5 pasi simpli</h1>
+			
+			<h3>Toata lumea stie ca Romania este o tara a tepelor, in care toata lumea vrea sa fure si sa pacaleasca pe
+ 			toata lumea. Din pacate, acest lucru se intampla si la masini, iar cei care vand la mana a doua automobile 
+ 			lovite si reparate prost, pe care le prezinta drept 'impecabile si nelovite', sunt destul de numerosi.
+ 			Iata cum sa recunoastem astfel de masini foarte usor.</h3>
+ 
+ 			<h3 class="h4" style='text-align:left;'>1. Masinile lovite in fata</h3>
+ 
+ 			<p style='font-size:120%;'>Mai bine de 50% dintre masinile lovite au avut parte de
+ 			accidente frontale. Acestea sunt destul de usor de identificat daca suntem putini atenti si urmarim 
+ 			liniile aripilor odata ce am ridicat capota motorului, traggerul, farurile si clemele, radiatorul si 
+ 			masca fata. Daca vedem urme de indoituri, de vopsea crapata, de vopsea noua sau de rugina, acolo au avut 
+ 			loc niste reparatii. Lonjeroanele sunt si ele predispuse la lovituri, asa ca e bine sa le verificam daca
+ 			cumva prezinta urme de suduri sau crapaturi.</p>
+ 
+ 			<a target="_blank" href="imagini/maslovfata.jpg"><img src="imagini/maslovfata.jpg"></a>
+ 
+ 			<h3 class="h4" style='text-align:left;'>2. Loviturile laterale</h3>
+  
+  			<p style='font-size:120%;'>Vreo 30% dintre masinile lovite au urme de reparatii in partile laterale. 
+  			Asta inseamna ca au avut parte de un accident cu un stalp sau copac sau o alta masina a intrat in lateral.
+  			Acest tip de lovituri sunt si mai usor de identificat: trebuie sa urmarim atent linia fiecarei portiere,
+  			linia fiecarui prag, acolo unde inchidem usile, liniile stalpilor de sustinere si cea a plafonului.
+  			Daca vedem urme de reparatii, de chituire, de suduri sau de vopsea care nu pare a fi originala, e clar
+  			ca acolo a avut loc un impact.</p>
+   			
+   			<a target="_blank" href="imagini/lovlaterale.jpg"><img src="imagini/lovlaterale.jpg"></a>
+  
+  			<h3 class="h4" style='text-align:left;'>3. Lovita in spate</h3>
+  
+  			<p style='font-size:120%;'>Unele masini care au fost lovite poarta urmele in partea... dorsala. 
+  			Acestea sunt destul de putine, dar exista. Ca sa identificam urmele unor lovituri din spate trebuie 
+  			sa ne uitam la linia portbagajului cu el deschis. Nu strica sa scoatem si capitonajele din portbagaj 
+  			pentru a vedea locul rotii de rezerva, in partile laterale, tabla podelei si la imbinarile dintre
+  			elemente. Daca vedem indoituri sau suduri, dar si urme de rugina, e clar ca acolo s-a intervenit. 
+  			Niciun tinichigiu din lume nu va reusi realinierea pieselor asa cum au venit ele din fabrica, la linie,
+  			asa ca un ochi avizat identifica usor reparatia.</p>
+   
+   			<a target="_blank" href="imagini/lovspate.jpg"><img src="imagini/lovspate.jpg"></a>
+  
+  			<h3 class="h4" style='text-align:left;'>4. Vopseaua care nu este originala</h3>
+   
+   			<p style='font-size:120%;'>De obicei, masini lovite si reparate au parte de o vopsire partiala sau 
+   			integrala. O masina revopsita se cunoaste destul de usor. Trebuie sa ne uitam la diferenta dintre 
+   			vopseaua din spatiul motorului si la cea exterioara, dar si la liniile de demarcatie izolate de 
+   			vopsitor cu banda. De obicei raman urme de gazare pe la chederele masinii, pe la oalele rotilor si
+   			pe langa balamalele portierelor. Poate ca masina a fost revopsita din cauza unor zgarieturi, nu a 
+   			unor lovituri, dar nu strica sa stim daca a fost revopsita.</p>
+   
+   			<h3 class="h4" style='text-align:left;'>5. Piesele aftermarket</h3>
+   
+   			<p style='font-size:120%;'>O masina care a fost lovita are multe sanse sa aiba piese aftermarket, 
+   			nu unele originale. Adica trebuie sa inspectam anumite elemente, precum faruri, stopuri, aripile, 
+   			grila, oglinzile, proiectoarele si asa mai departe, iar daca nu au stanta originala sau vedem diverse
+   			semne care semnaleaza fabricarea in china si o calitate indoielnica, e clar ca vorbim de elemente 
+   			aftermarket. Trebuie sa ne punem un semn de intrebare daca o masina nu mai are un tragger original 
+   			sub capota si eventual chiar unul de alta culoare, in locul celui original.</p>
+   
+   			<h3>Concluzia este ca daca esti putin atent, poti sa-ti dai seama daca un automobil a fost lovit si 
+   			reparat. Masina poate sa fi fost reparata bine, profesional, dar in cazul in care vanzatorul a spus 
+   			in anunt ca masina e nelovita, aceasta mica scapare te poate ajuta sa obtii un pret mai bun. 
+   			Si daca masina a fost lovita mai zdravan si reparata, poti verifica elemente importante precum 
+   			airbaguri, lonjeroane sau alte piese de rezistenta de care depinde siguranta autoturismului in trafic.</h3>
+   
+   			<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+		</article>
 	</div>
-	</div>
-<div style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
-<img src="imagini/masinalovita.jpg">
-<article>
-<h1 style='color:#0066cc;'>Cum sa recunoastem o masina lovita si reparata in doar 5 pasi simpli</h1>
-<h3>Toata lumea stie ca Romania este o tara a tepelor, in care toata lumea vrea sa fure si sa pacaleasca pe
- toata lumea. Din pacate, acest lucru se intampla si la masini, iar cei care vand la mana a doua automobile 
- lovite si reparate prost, pe care le prezinta drept 'impecabile si nelovite', sunt destul de numerosi.
- Iata cum sa recunoastem astfel de masini foarte usor.</h3>
- <h3 class="h4" style='text-align:left;'>1. Masinile lovite in fata</h3>
- <p style='font-size:120%;'>Mai bine de 50% dintre masinile lovite au avut parte de
- accidente frontale. Acestea sunt destul de usor de identificat daca suntem putini atenti si urmarim 
- liniile aripilor odata ce am ridicat capota motorului, traggerul, farurile si clemele, radiatorul si 
- masca fata. Daca vedem urme de indoituri, de vopsea crapata, de vopsea noua sau de rugina, acolo au avut 
- loc niste reparatii. Lonjeroanele sunt si ele predispuse la lovituri, asa ca e bine sa le verificam daca
- cumva prezinta urme de suduri sau crapaturi.</p>
- <a target="_blank" href="imagini/maslovfata.jpg"><img src="imagini/maslovfata.jpg"></a>
- <h3 class="h4" style='text-align:left;'>2. Loviturile laterale</h3>
-  <p style='font-size:120%;'>Vreo 30% dintre masinile lovite au urme de reparatii in partile laterale. 
-  Asta inseamna ca au avut parte de un accident cu un stalp sau copac sau o alta masina a intrat in lateral.
-  Acest tip de lovituri sunt si mai usor de identificat: trebuie sa urmarim atent linia fiecarei portiere,
-  linia fiecarui prag, acolo unde inchidem usile, liniile stalpilor de sustinere si cea a plafonului.
-  Daca vedem urme de reparatii, de chituire, de suduri sau de vopsea care nu pare a fi originala, e clar
-  ca acolo a avut loc un impact.</p>
-   <a target="_blank" href="imagini/lovlaterale.jpg"><img src="imagini/lovlaterale.jpg"></a>
-  <h3 class="h4" style='text-align:left;'>3. Lovita in spate</h3>
-  <p style='font-size:120%;'>Unele masini care au fost lovite poarta urmele in partea... dorsala. 
-  Acestea sunt destul de putine, dar exista. Ca sa identificam urmele unor lovituri din spate trebuie 
-  sa ne uitam la linia portbagajului cu el deschis. Nu strica sa scoatem si capitonajele din portbagaj 
-  pentru a vedea locul rotii de rezerva, in partile laterale, tabla podelei si la imbinarile dintre
-  elemente. Daca vedem indoituri sau suduri, dar si urme de rugina, e clar ca acolo s-a intervenit. 
-  Niciun tinichigiu din lume nu va reusi realinierea pieselor asa cum au venit ele din fabrica, la linie,
-  asa ca un ochi avizat identifica usor reparatia.</p>
-   <a target="_blank" href="imagini/lovspate.jpg"><img src="imagini/lovspate.jpg"></a>
-  <h3 class="h4" style='text-align:left;'>4. Vopseaua care nu este originala</h3>
-   <p style='font-size:120%;'>De obicei, masini lovite si reparate au parte de o vopsire partiala sau 
-   integrala. O masina revopsita se cunoaste destul de usor. Trebuie sa ne uitam la diferenta dintre 
-   vopseaua din spatiul motorului si la cea exterioara, dar si la liniile de demarcatie izolate de 
-   vopsitor cu banda. De obicei raman urme de gazare pe la chederele masinii, pe la oalele rotilor si
-   pe langa balamalele portierelor. Poate ca masina a fost revopsita din cauza unor zgarieturi, nu a 
-   unor lovituri, dar nu strica sa stim daca a fost revopsita.</p>
-   <h3 class="h4" style='text-align:left;'>5. Piesele aftermarket</h3>
-   <p style='font-size:120%;'>O masina care a fost lovita are multe sanse sa aiba piese aftermarket, 
-   nu unele originale. Adica trebuie sa inspectam anumite elemente, precum faruri, stopuri, aripile, 
-   grila, oglinzile, proiectoarele si asa mai departe, iar daca nu au stanta originala sau vedem diverse
-   semne care semnaleaza fabricarea in china si o calitate indoielnica, e clar ca vorbim de elemente 
-   aftermarket. Trebuie sa ne punem un semn de intrebare daca o masina nu mai are un tragger original 
-   sub capota si eventual chiar unul de alta culoare, in locul celui original.</p>
-   <h3>Concluzia este ca daca esti putin atent, poti sa-ti dai seama daca un automobil a fost lovit si 
-   reparat. Masina poate sa fi fost reparata bine, profesional, dar in cazul in care vanzatorul a spus 
-   in anunt ca masina e nelovita, aceasta mica scapare te poate ajuta sa obtii un pret mai bun. 
-   Si daca masina a fost lovita mai zdravan si reparata, poti verifica elemente importante precum 
-   airbaguri, lonjeroane sau alte piese de rezistenta de care depinde siguranta autoturismului in trafic.</h3>
-   <p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
-</article>
 </div>
-</div>
+
 <?php
 include("manage_comments.php");
 include("display_comments.php");
