2769ac2b3f4560f514e143cdf157eae3378a02dc
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-20 13:23:46 +0300

    Update stire6.php

--- stire6.php
+++ stire6.php
@@ -16,21 +16,28 @@
 		<img src="imagini/capacitcilindrica.jpg">
 
 		<article>
-<h1 style='color:#0066cc;'>Tu stii cum se calculeaza capacitatea cilindrica a unui motor?</h1>
-<h3>Cu totii avem automobile, scutere, motociclete sau barci de agrement. Si atata timp cat sunt
- dotate cu motoare termice, ele sunt catalogate in functie de capacitatea cilindrica a acestora. 
- Dar cum se calculeaza ea?</h3>
- <p style='font-size:120%;'>Capacitatea cilindrica se mai numeste litraj si este o unitate de masura a 
- motoarelor cu ardere interna. Capacitatea cilindrica, in linii mari, ne arata volumul pe care il are
- un bloc motor in spatiul pistoanelor. Cu alte cuvinte, cu cat volumul unui motor este mai mare, cu atat
- capacitatea este mai mare. Si pentru ca vorbim de volum, nu degeaba se masoara, in Europa, in litri
- sau in centimetri cubi.</p>
- <a target="_blank" href="imagini/capacitcil1.jpg"><img src="imagini/capacitcil1.jpg"></a>
- <p style='font-size:120%;text-align:left;'>Formula de calcul a unei capacitati cilindrice este urmatoarea:</p>
- <p style='font-size:120%;text-align:left;'><b>Pi/4 X alezaj la puterea a doua X cursa X numarul de cilindri</b></p>
- <p style='font-size:120%;'>Alezajul (Bore in engleza) inseamna diametrul unui cilindru calculat in
- centimetri. Cursa (Stroke in engleza) este distanta pe care o face pistonul din pozitia cea mai joasa
- in pozitia cea mai inalta, calculata in centimetri.</p>
+			<h1 style='color:#0066cc;'>Tu stii cum se calculeaza capacitatea cilindrica a unui motor?</h1>
+
+			<h3>Cu totii avem automobile, scutere, motociclete sau barci de agrement. Si atata timp cat sunt
+ 			dotate cu motoare termice, ele sunt catalogate in functie de capacitatea cilindrica a acestora. 
+ 			Dar cum se calculeaza ea?</h3>
+ 
+ 			<p style='font-size:120%;'>Capacitatea cilindrica se mai numeste litraj si este o unitate de masura a 
+ 			motoarelor cu ardere interna. Capacitatea cilindrica, in linii mari, ne arata volumul pe care il are
+ 			un bloc motor in spatiul pistoanelor. Cu alte cuvinte, cu cat volumul unui motor este mai mare, cu atat
+ 			capacitatea este mai mare. Si pentru ca vorbim de volum, nu degeaba se masoara, in Europa, in litri
+ 			sau in centimetri cubi.</p>
+ 
+ 			<a target="_blank" href="imagini/capacitcil1.jpg"><img src="imagini/capacitcil1.jpg"></a>
+ 
+ 			<p style='font-size:120%;text-align:left;'>Formula de calcul a unei capacitati cilindrice este urmatoarea:</p>
+ 			
+ 			<p style='font-size:120%;text-align:left;'><b>Pi/4 X alezaj la puterea a doua X cursa X numarul de cilindri</b></p>
+ 
+ 			<p style='font-size:120%;'>Alezajul (Bore in engleza) inseamna diametrul unui cilindru calculat in
+ 			centimetri. Cursa (Stroke in engleza) este distanta pe care o face pistonul din pozitia cea mai joasa
+ 			in pozitia cea mai inalta, calculata in centimetri.</p>
+ 
  <p style='font-size:120%;text-align:left;'>Volumul unui piston se calculeaza:<b> Pi/4 X alezaj la puterea a doua X cursa</b></p>
  <p style='font-size:120%;'>De exemplu, sa spunem ca un cilindru are un alezaj (diametru) de 10 centimetri
  si o cursa de 4 centimetri. Calculand dupa formula urmatoare ne iese capacitatea cilindrica:</p>
