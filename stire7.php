1435cea8cf3f60f6018b5a488a0d148a9d27492d
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-23 14:28:55 +0300

    Create stire7.php

---
+++ stire7.php
@@ -0,0 +1,62 @@
+<div style='background-color:black;'>
+ 	
+ 	<div id="header" style='width: 300px;height: 10px;margin: 0px auto;'>
+
+		<div id="logo"style='float:left;'>	
+			<a href="index.html" style='color:#FFFFFF;text-decoration: none;'><h1>BestCars</h1></a>
+		</div>
+		
+		<div id="logocar" style='float:right;'>	
+			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
+		</div>
+	</div>
+	
+	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+
+		<img src="imagini/imagine_stire7.jpg">
+
+		<article>
+			<h1 style='color:#5CABFF;'>Peste 7000 de clienti si-au exprimat dorinta de a cumpara unul din cele 500 de Forduri GT</h1>
+
+			<h3>Ford a anuntat ca va realiza numai 500 de exemplare din cel mai nou hypercar american, modelul GT, 
+ 			aflat deja la a 3-a generatie. Insa interesul clientilor pentru aceasta masina se pare ca este urias, in  
+ 			ciuda pretului colosal al masinii: peste 7000 de persoane deja se bat pentru a lua unul din putinele GT-uri 
+			care se vor vinde incepand de anul viitor.</h3>
+ 
+ 			<p style='font-size:120%;'>Ford a demarat inscrierile pentru cumpararea unui Ford GT din care a anuntat ca va face doar 500 de unitati, cate 250 pe an in  
+ 			2017 si 2018. Insa interesul opiniei publice se dovedeste a fi peste asteptarile producatorilor americani, deoarece, in 
+ 			doar cateva zile, peste 7000 de persoane s-au inscris in speranta ca vor apuca sa achizitioneze super-masina 
+ 			americana.</p>
+ 
+ 			<a target="_blank" href="imagini/imagine_stire7-2.jpg"><img src="imagini/imagine_stire7-2.jpg"></a>
+ 			
+ 			<p style='font-size:120%;text-align:left;'><b>Pi/4 X alezaj la puterea a doua X cursa X numarul de cilindri</b></p>-->
+ 
+ 			<p style='font-size:120%;'>Supercarul cu motor central de 3.5 litri V6 EcoBoost va avea peste 600 cp, insa alte detalii nu se cunosc, deoarece Ford 
+ 			a pastrat atat acest model secret, cat si specificatiile finale. Oricum, fara sa-i intereseze de performante, o sumedenie de 
+ 			fani ai modelului, colectionari, dar si samsari care vor sa scoata profit din aceasta masina rara si scumpa s-au inscris pe pagina 
+			oficiala Ford ca sa cumpere un exemplar. Comenzile preliminare se vor incheia pe 12 mai, dupa care va urma o lunga 
+			asteptare pentru a vedea cine a fost ales sa conduca masina.</p>
+ 
+ 			
+ 
+ 			<p style='font-size:120%;'>Dar aceasta ingramadire la noul Ford GT nu prea poate fi explicata. Mai ales daca ne gandim ca masina, chiar daca va 
+ 			fi produsa in doar 500 de unitati, va avea un pret de 450.000 de dolari in America si 500.000 de Euro in Europa. Adica o 
+			suma uriasa, mai mare decat ar plati cineva pentru un Lamborghini Aventador, un Mercedes-Benz SLS 63 AMG sau un Ferrari 
+			F12. Chiar si asa, se pare ca banii nu sunt o problema pentru cei 7000 de oameni care s-au inscris pe lista de comenzi.
+			</p>
+ 
+ 			
+ 	
+ 			<a target="_blank" href="imagini/imagine_stire7-3.jpg"><img src="imagini/imagine_stire7-3.jpg"></a>
+			
+			<p style='font-size:120%;'>Ford a tinut insa sa sublinieze ca va alege clientii pe spranceana. Si a spus ca au prioritate cei care au de gand sa conduca 
+ 			masina, care stiu sa aiba grija de ea si care au de gand sa o pastreze, spunand ca la coada listei vor fi plasati cei care vor sa o 
+			cumpere ca sa o vanda mai tarziu cu un profit serios. Adica pilotii si colectionarii au intaietate, iar samsarii, fie ei si 
+			miliardari, cel mai probabil se vor alege cu buza umflata...
+			</p>
+ 
+ 			<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+		</article>
+	</div>
+</div>
