5019ca8955d930fc9526e28b43977bfce24c39f3
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-23 19:11:53 +0300

    Update stire1.php

--- stire1.php
+++ stire1.php
@@ -11,134 +11,102 @@
 		<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
 	</div>
 </div>
+<?php
+$con = mysql_connect('localhost','root','');
+ 
+if (!$con)
+{
+  die('Could not connect: ' . mysql_error());
+}
+ 
+mysql_select_db('auto', $con);
 
-<div id="stire" style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
-	<img src="imagini/stire1.jpg">
+$query = mysql_query("SELECT * FROM stiri WHERE id = 1") or die(mysql_error());
+while($row = mysql_fetch_array($query)){
+	$titlu = $row['titlu'];		$text11 = $row['text11'];		$text22 = $row['text22'];
+	$text1 = $row['text1'];		$text12 = $row['text12'];		$text23 = $row['text23'];
+	$text2 = $row['text2'];		$text13 = $row['text13'];		$text24 = $row['text24'];
+	$text3 = $row['text3'];		$text14 = $row['text14'];		$text25 = $row['text25'];
+	$text4 = $row['text4'];		$text15 = $row['text15'];		$text26 = $row['text26'];
+	$text5 = $row['text5'];		$text16 = $row['text16'];		$text27 = $row['text27'];
+	$text6 = $row['text6'];		$text17 = $row['text17'];		$text28 = $row['text28'];
+	$text7 = $row['text7'];		$text18 = $row['text18'];		$text29 = $row['text29'];
+	$text8 = $row['text8'];		$text19 = $row['text19'];		$text30 = $row['text30'];
+	$text9 = $row['text9'];		$text20 = $row['text20'];		$text31 = $row['text31'];
+	$text10 = $row['text10'];	$text21 = $row['text21'];		
+	
+}
+echo " <div  style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
+	<img src='imagini/stire1.jpg'>
 	<article>
-		<h1 style='color:#0066cc;'>Specialistii ne invata: cum alegem anvelopele potrivite si cum<br> verificam masina la sfarsit de iarna</h1>
+		<h1 style='color:#0066cc;'>$titlu</h1>
 		
-		<h3>Acum ca iarna si-a cam iesit din drepturi si meteorologii ne anunta ca urmeaza zile calduroase, tot mai multi soferi se pregatesc sa-si schimbe pneurile de iarna cu cele de vara. Indiferent ca vrei niste anvelope noi sau ca le pui pe cele din magazie, specialistii de la Top Lac Auto ne invata cum sa trecem de la un sezon la altul cu automobilul pentru a ne bucura de o maxima siguranta.</h3>
+		<h3>$text1</h3>
 		
-		<h3 class="h4" style='text-align:left;'><b>1. Alegerea corecta a anvelopelor</b></h3>
+		<h3  style='text-align:left;'>$text2</h3>
 		
-		<p style='font-size:120%;text-indent: 50px;'>Daca dupa sezonul rece trebuie sa achizitionam un set de vara nou, 
-		trebuie sa luam in calcul cateva aspecte. <b>In primul rand,
- 		trebuie sa ne asiguram ca vom achizitiona pneurile potrivite pentru masina noastra.</b> 
-		Adica anvelope fabricate pentru o anumita viteza maxima, pentru un anumit grad de incarcatura si 
- 		care apartin unui brand recomandat de producator. In plus, trebuie sa tinem cont de ceea ce ne dorim: 
- 		siguranta, longevitate, economie de carburant sau performanta. Si in acest caz trebuie sa luam in calcul 
- 		insemnele de pe etichetele care sunt acum pe toate anvelopele comercializate.<b> ATENTIE!</b> Daca o anvelopa nu 
- 		are eticheta europeana cu cele 3 criterii de zgomot, franare pe carosabil umed si eficienta, nu trebuie
- 		cumparata!</p>
+		<p style='font-size:120%;text-indent: 50px;'>$text3</p>
  		
- 		<a target="_blank" href="imagini/etichetaue.jpg"><img src="imagini/etichetaue.jpg"></a>
+ 		<a target='_blank' href='$text4'><img src='$text4'></a>
  		
- 		<p id="border" style='color:white;font-size:120%;'>Eticheta EU ne arata eficienta, franarea pe carosabil umed si nivelul de zgomot</p>
+ 		<p  style='color:white;font-size:120%;'>$text5</p>
  
- 		<h3 class="h4" style='text-align:left;'><b>2. Echilibrarea rotilor la fiecare inceput de sezon</b></h3>
+ 		<h3  style='text-align:left;'>$text6</h3>
  		
- 		<p style='font-size:120%;text-indent: 50px;'>Nu conteaza daca avem un set suplimentar de vara fata de cel de iarna, cu jante si 
- 		anvelope separate, sau daca avem doar anvelopele pe care le montam pe acelasi set de jante. 
- 		<b>Atunci cand facem schimbul, fie ca este de vara sau de iarna, specialistii ne recomanda sa facem o
- 		echilibrare a celor 4 roti.</b> Pentru ca sunt sanse ca, de-a lungul folosintei, ori plumbii sa fi cazut, 
- 		ori jantele sa se fi strambat sau pneurile sa aiba galme sau deformari din pricina gropilor. 
- 		Asa ca este recomandat sa echilibram si daca montam doar anvelopele, dar si daca montam setul complet 
- 		jante + anvelope, ca sa ne asiguram ca nu o sa avem probleme in sezonul cald care urmeaza.</p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text7</p>
  
- 		<h3 class="h4"style='text-align:left;'><b>3. Presiunea perfecta in anvelope</b></h3>
+ 		<h3 style='text-align:left;'>$text8</h3>
  
- 		<p style='font-size:120%;text-indent: 50px;'>Multi soferi din Romania omit sa verifice presiunea din anvelope si circula fara sa umble la roti
- 		cu luni sau chiar ani. Mereu trebuie sa ne asiguram ca avem in pneuri presiunea recomandata de 
- 		producator sau inscrisa pe automobilul nostru, in zona stalpului dintre portiere. <b>Daca vom circula 
- 		cu presiune mai mica vom uza anvelope neuniform si aceasta se va incalzi mai repede, iar daca avem 
- 		presiune prea mare, la viteza csrecuta, din cauza ca aerul se dilata, riscam sa avem parte de o explozie.</b>
- 		Asadar, nu strica sa ne uitam cel putin o data pe luna la presiunea din rotile noastre.</p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text9</p>
  		
- 		<a target="_blank" href="imagini/anvelope.jpg"><img src="imagini/anvelope.jpg"></a>
+ 		<a target='_blank' href='$text10'><img src='$text10'></a>
  
- 		<p id="border"style='color:white;font-size:120%;'>Putem sa verificam presiunea si singuri, in orice benzinarie</p>
+ 		<p style='color:white;font-size:120%;'>$text11</p>
  
- 		<h3 class="h4"style='text-align:left;'><b>4. Cauciucurile all-season, un compromis nedorit</b></h3>
+ 		<h3 style='text-align:left;'>$text12</h3>
  
- 		<p style='font-size:120%;text-indent: 50px;'>Anvelopele dedicate folosirii anului intreg, numite mixte sau all-season, sunt bune la tot
- 		si bune la nimic. Adica au un profil jumatate de iarna, jumatate de vara, ceea ce inseamna ca nu se
- 		vor comporta excelent nici vara, nici iarna. In plus, materialul din care sunt facute este cel folosit
- 		la anvelopele de iarna, ceea ce inseamna ca acestea se vor uza mai repede vara. Si cum in Romania zilele
- 		calduroase sunt mai mult decat cele geroase, anvelopele all-season nu prea sunt de dorit.<b> Acum au aparut
- 		si anvelope de vara, omologate si pentru iarna, care sunt mult mai bune, insa ideal este sa avem cate un
- 		set pentru fiecare sezon daca vrem sa ne asiguram de franare, longevitate si aderenta optime, fie ca este
- 		canicula, fie ca este polei si ger.</b></p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text13</p>
  
- 		<h3 class="h4"style='text-align:left;'><b>5. Anvelopa dedicata sezonului in care suntem</b></h3>
+ 		<h3 style='text-align:left;'>$text14</h3>
  
- 		<p style='font-size:120%;text-indent: 50px;'>Producatorii de pneuri nu au facut degeaba doua tipuri, in functie de sezon, adica pneuri de vara
- 		si pneuri de iarna. Ci pentru ca au vrut sa ofere siguranta maxima soferilor din toata lumea.
- 		<b>Asadar, indiferent ca ninge sau nu, iarna este considerat sezonul in care temperatura medie a unei 
- 		saptamani scade sub 7 grade Celsius.</b> Cand termometrul incepe sa ne arate mai putin, e timpul sa trecem 
- 		la anvelopele de iarna. Acelasi lucru se intampla prin lunile martie, aprilie: cand vedem ca temperatura
- 		zilei este de peste 7 grade Celsius, e timpul sa trecem la setul de vara. <b>Orice tentativa de a 'pacali'
- 		acest sistem eficient poate duce la accidente grave.</b></p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text15</p>
  
- 		<a target="_blank" href="imagini/cauciucuri.jpg"><img src="imagini/cauciucuri.jpg"></a>
+ 		<a target='_blank' href='$text16'><img src='$text16'></a>
  		
- 		<p id="border"style='color:white;font-size:120%;'>Anvelopa de vara (stanga) vs. anvelopa de iarna (dreapta)</p>
+ 		<p style='color:white;font-size:120%;'>$text17</p>
  		
- 		<h3 class="h4"style='text-align:left;'><b>6. Mai putin de 1,6 mm profil ne poate costa viata</b></h3>
+ 		<h3 style='text-align:left;'>$text18</h3>
  		
- 		<p style='font-size:120%;text-indent: 50px;'><b>Conform legislatiei in vigoare, Registrul Auto Roman spune ca o anvelopa nu are voie sa fie 
- 		utilizata daca are profil mai mic de 1,6 mm.</b> Noi credem ca este putin si ca profilul trebuie sa fie
- 		mai mare pentru franare optima, inclusiv in cazul pneurilor de iarna care daca au mai putin de 5 mm 
- 		in carne, nu mai sunt anvelope de iarna! Daca insa nu ne intereseaza siguranta noastra si a pasagerilor,
- 		e bine sa respectam legea si sa ne asiguram ca avem mereu anvelope cu profil de peste 1,6 mm, in caz 
- 		contrar riscam amenzi usturatoare. <b>Mai mult, daca suntem implicati intr-un accident, indiferent ca suntem
- 		pagubit sau vinovat, riscam ca societatea de asigurari sa nu ne despagubeasca din cauza anvelopelor uzate!</b></p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text19</p>
  
- 		<h3 class="h4"style='text-align:left;'><b>7. Geometria, recomandata cel putin o data pe an</b></h3>
+ 		<h3 style='text-align:left;'>$text20</h3>
  		
- 		<p style='font-size:120%;text-indent: 50px;'>Specialistii ne recomanda sa facem geometria rotilor cel putin o data pe an, de preferat la sfarsit 
- 		de iarna, atunci cand ne montam pneurile de vara. Pentru ca din cauza gropilor, din cauza anvelopelor 
- 		dezechilibrate, din cauza bordurilor si a sinelor de tramvai, sistemul de directie al masinii are de 
- 		suferit. <b>Exista riscul ca unele suruburi sa se slabeasca, unele piese sa se indoaie si unele conexiuni 
- 		din cauciuc, bucse sau pivoti, sa cedeze.</b> Aceste lucruri apar in general la sfarsit de iarna, asa ca 
- 		odata cu montarea noului set de vara, ca sa ne asiguram ca masina este in regula si ca nu va uza pneurile
- 		neuniform, e bine sa facem o geometrie.</p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text21</p>
 
-		<a target="_blank" href="imagini/geometrie.jpg"> <img src="imagini/geometrie.jpg"></a>
+		<a target='_blank' href='$text22'> <img src='$text22'></a>
  
- 		<p id="border" style='color:white;font-size:120%;'>O geometrie computerizata ne scapa de uzura neuniforma a pneurilor</p>
+ 		<p  style='color:white;font-size:120%;'>$text23</p>
  
- 		<h3 class="h4"style='text-align:left;'><b>8. Verificarea lichidelor si a bateriei auto</b></h3>
+ 		<h3 style='text-align:left;'>$text24</h3>
  
- 		<p style='font-size:120%;text-indent: 50px;'>Tot un sfat la inceputul sezonului cald, ca sa evitam problemele din concediu, este sa ne uitam la 
- 		nivelul si calitatea lichidelor de sub capota motorului. <b>Ideal ar fi sa mergem intr-un service 
- 		specializat care este dotat cu aparate de masura.</b> Pentru ca trebuie sa verificam lichidul de frana,
- 		daca este la nivel si daca mai este bun, antigelul, daca este la nivel si daca mai are eficienta, 
- 		lichidul de servo si chiar uleiul motorului. <b>In plus, acumulatorul masinii, dupa o iarna grea, are 
- 		toate sansele sa cedeze. Un test cu un aparat dedicat, care ne spune daca bateria noastra inmagazineaza
- 		corect curent, este ideal, ca sa nu ne trezim pe autostrada, cand mergem spre litoral, ca nu mai avem
- 		curent.</b></p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text25</p>
  
- 		<a target="_blank" href="imagini/lichide.jpg"><img src="imagini/lichide.jpg"></a>
+ 		<a target='_blank' href='$text26'><img src='$text26'></a>
  		
- 		<h3 class="h4"style='text-align:left;'><b>9. Inspectie GRATUITA a masinii la final de iarna</b></h3>
+ 		<h3 style='text-align:left;'>$text27</h3>
  
- 		<p style='font-size:120%;text-indent: 50px;'>Tot din aceleasi motive ca cele enumerate mai sus, cum ar fi gropi, borduri, 
- 		dar si fortarea masinii iarna, cand ramanem impotmoliti sau uzura suplimentara cauzata de nisip,
- 		sare si substantele aruncate pe carosabil, sunt sanse sa ne trezim la masina cu anumite probleme. 
- 		Fie la suspensii, fie la directie sau chiar la franare. Pentru a ne asigura ca vara care tocmai incepe
- 		va fi una lipsita de emotii si ca in concedii nu trebuie sa ne facem griji ca ramanem cu masina in drum,
- 		e bine sa facem o inspectie mecanica gratuita. Astfel de inspectii sunt oferite in general de cateva
- 		service-uri mari care tin la siguranta soferilor, cum este si cazul <a href="http://www.toplacservice.ro/" style='text-decoration: none;color:white;'>Top Lac Auto.</a></p>
+ 		<p style='font-size:120%;text-indent: 50px;'>$text28</p>
  
- 		<a target="_blank" href="imagini/toplacauto.jpg"><img src="imagini/toplacauto.jpg"></a>
+ 		<a target='_blank' href='$text29'><img src='$text29'></a>
  
- 		<p id="border" style='color:white;font-size:120%;'>Echipa Top Lac Auto</p>
+ 		<p  style='color:white;font-size:120%;'>$text30</p>
  
- 		<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+ 		<p  style='text-align:left;font-size:110%;'>$text31</p>
  	</article>
 </div>
 </div>
-
+";
+?>
 <?php
 include("manage_comments.php");
 include("display_comments.php");
