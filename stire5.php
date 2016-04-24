e9255fd42e267a2d643a7a68ddacfbafdb7b5117
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-24 19:05:46 +0300

    Update stire5.php

--- stire5.php
+++ stire5.php
@@ -10,92 +10,76 @@
 			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
 		</div>
 	</div>
+	<?php
+	$con = mysql_connect('localhost','root','');
+ 
+if (!$con)
+{
+  die('Could not connect: ' . mysql_error());
+}
+ 
+mysql_select_db('auto', $con);
+
+$query = mysql_query("SELECT * FROM stiri WHERE id = 5") or die(mysql_error());
+while($row = mysql_fetch_array($query)){
+	$titlu = $row['titlu'];		$text11 = $row['text11'];		
+	$text1 = $row['text1'];		$text12 = $row['text12'];		
+	$text2 = $row['text2'];		$text13 = $row['text13'];		
+	$text3 = $row['text3'];		$text14 = $row['text14'];		
+	$text4 = $row['text4'];		$text15 = $row['text15'];		
+	$text5 = $row['text5'];		$text16 = $row['text16'];		
+	$text6 = $row['text6'];		$text17 = $row['text17'];		
+	$text7 = $row['text7'];			
+	$text8 = $row['text8'];				
+	$text9 = $row['text9'];			
+	$text10 = $row['text10'];			
 	
-	<div style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
+}
+	echo "<div style='text-align:center;color:#999999;width: 950px;margin: 0px auto;'>
 		
-		<img src="imagini/faruri.jpg">
+		<img src='imagini/faruri.jpg'>
 		
 			<article>
-				<h1 style='color:#0066cc;'>Imposibil de omologat? Ce elemente trebuie sa aiba farurile cu Xenon ca<br> sa circulam in legalitate?</h1>
+				<h1 style='color:#0066cc;'>$titlu</h1>
 
-				<h3>Internetul este plin cu sisteme aftermarket cu xenon, scumpe sau ieftine, pe care soferii din Romania 
-				le cumpara avand impresia ca fac o afacere buna. Din pacate, in tara noastra este aproape imposibil sa iti
- 				montezi xenon pe masina si sa respecti legea, atata timp cat masina nu a venit din fabrica cu asa ceva.</h3>
+				<h3>$text1</h3>
 
-				<p style='font-size:120%;'>Soferii romani, extrem de prost informati, au impresia ca isi pot schimba sursa
- 				de lumina de pe propriile automobile dupa bunul plac. Din nefericire pentru acestia, <b>Registrul Auto
- 				Roman interzice montarea unui alt tip de iluminare fara ca acesta sa nu fie omologat si mentionat in 
- 				cartea de identitate a autoturismului.</b></p>
+				<p style='font-size:120%;'>$text2</p>
  
- 				<p style='font-size:120%;'>Cazuri de soferi care si-au montat in farurile masinii kituri de xenon care
- 				costa sub 100 de lei sunt cu miile. Toti acesti soferi insa incalca legea si risca oricand sa ramana 
- 				fara talon si nu pot obtine legal ITP-ul sub nicio forma. <b>La fel de multi sunt si cei care au impresia 
- 				ca isi pot monta instalatii de xenon daca farurile lor au lupe, ceea ce este total gresit.</b></p>
+ 				<p style='font-size:120%;'>$text3</p>
   
-  				<p style='font-size:120%;'><b>Inlocuirea sursei de iluminare a masinii cu un kit xenon, conform spuselor 
-  				oficialilor RAR, se face doar in conditii speciale, extrem de greu de indeplinit de majoritatea soferilor.</b>
-  				Ca sa respecte legea, un kit cu xenon trebuie neaparat sa fie format din:<span style='text-decoration:underline;'> faruri specifice, surse de
-  				lumina cu descarcare in gaz, dispozitiv care declanseaza arcul electric (ignitor) si cel care 
-  				stabilizeaza curentul electric (ballast), sistem automat de reglaj in functie de sarcina, spalator far.</span></p>
+  				<p style='font-size:120%;'>$text4</p>
  
- 				<p style='font-size:120%;'><b>Cu alte cuvinte, daca tu ai pe masina faruri cu xenon, facute de tine
- 				prin simpla montare a unor becuri, sau cine stie ce chinezarii ordinare, fara sa ai toate cele 
- 				mentionate mai sus, risti sa iti pierzi talonul si sa fii chemat la RAR.</b></p>
+ 				<p style='font-size:120%;'>$text5</p>
  
- 				<p style='font-size:120%;'><b>Cine doreste sa fie in legalitate are doar doua variante. Prima este sa-si 
- 				cumpere sistemul de iluminare original, special pentru un anume model de masina, sistem care a fost
- 				montat pe vehicule similare,</b> dar care sa cuprinda obligatoriu faruri specifice, surse de lumina cu 
- 				descarcare in gaz, dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza
- 				curentul electric (ballast), sistem automat de reglaj in functie de sarcina, spalator far. 
- 				Adica sa aiba far cu lupa, becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri. 
- 				<b>Din fericire, toate farurile OEM cu xenon au asa ceva.</b></p>
+ 				<p style='font-size:120%;'>$text6</p>
  
- 				<a target="_blank" href="imagini/faruribmw.jpg"><img src="imagini/faruribmw.jpg"></a>
+ 				<a target='_blank' href='$text7'><img src='$text7'></a>
  
- 				<p id="border" style='color:white;font-size:120%;'>Faruri originale de BMW cu xenon din fabrica</p>
+ 				<p  style='color:white;font-size:120%;'>$text8</p>
  
- 				<p style='font-size:120%;'><b>Montarea farurilor cu xenon OEM, adica originale, fie ele noi, fie de la 
- 				dezmembrari, trebuie sa se faca exclusiv intr-un centru autorizat al reprezentantei constructorului 
- 				in Romania. </b>Adica daca vrei sa-ti pui xenon pe un Seria 3 E46, trebuie sa mergi la Automobile Bavaria.
- 				Acestia trebuie sa-ti dea o adresa in care sunt enumerate elementele montate si se mentioneaza ca 
- 				sunt originale, de prim montaj.</p>
+ 				<p style='font-size:120%;'>$text9</p>
   
-  				<p style='font-size:120%;'>Cu aceasta adresa de la service, esti obligat sa te duci la Registrul Auto
-  				Roman pentru a le omologa si pentru a le trece in cartea de identitate. Asta costa circa 150 de lei.</p>
+  				<p style='font-size:120%;'>$text10</p>
    
-   				<p style='font-size:120%;'><b>O alta varianta este montarea unor faruri aftermarket.</b> Acestea trebuie 
-   				neaparat sa fie montate cu kitul obligatoriu: faruri specifice, surse de lumina cu descarcare in gaz,
-   				dispozitiv care declanseaza arcul electric (ignitor) si cel care stabilizeaza curentul electric 
-   				(ballast), sistem automat de reglaj in functie de sarcina, spalator far. Adica sa aiba far cu lupa,
-   				becuri xenon, ignitor, ballast, sistem autonivelant si spalator faruri.</p>
+   				<p style='font-size:120%;'>$text11.</p>
    
-   				<p style='font-size:120%;'>Montarea farurilor cu xenon aftermarket, numite oficial retrofit, 
-   				este obligatoriu sa fie realizata intr-un service omologat pentru modificari constructive. 
-   				Acest service trebuie sa-ti ofere la finalul montajului o declaratie de conformitate emisa de 
-   				inginerii de acolo, in care sunt enumerate elementele montate, cu specificarea numerelor de omologare
-   				CE/ECE-ONU pentru fiecare element in parte.</p>
+   				<p style='font-size:120%;'>$text12</p>
     
-    				<p style='font-size:120%;'><b>Ulterior, cei care si-au pus pe masina faruri cu xenon aftermarket, 
-				trebuie sa mearga la o reprezentanta RAR pentru a mentionarea in carte: “Echipare retrofit cu surse 
-				de lumină cu descărcare în gaz”. Asta inseamna modificare in carte care costa circa 150 de lei.</b></p>
+    				<p style='font-size:120%;'>$text13</p>
  
- 				<p style='font-size:120%;'>Cine este prins de autoritati ca circula cu o masina care are faruri cu xenon
- 				ce nu respecta aceste reguli, ramane fara talon si primeste o sanctiune de 1-2 puncte amenda, adica 
- 				intre 105 si 210 lei. Mai mult, cine ajunge la RAR pentru alte operatiuni, cum ar fi identificarea in
- 				vederea inmatricularii, va fi respins si cartea retinuta pana la modificarea farurilor. Cat despre ITP,
- 				un loc in care se face Inspectia Tehnica Periodica care se respecta si care respecta legea, nu va 
- 				accepta automobile care au sistemul de lumini din fabrica inlocuit.</p>
+ 				<p style='font-size:120%;'>$text14</p>
  
- 				<a target="_blank" href="imagini/kitfar.jpg"><img src="imagini/kitfar.jpg"></a>
+ 				<a target='_blank' href='$text15'><img src='$text15'></a>
  
- 				<p id="border" style='color:white;font-size:120%;'>Asemenea kituri sunt incomplete pentru cei care vor sa si 
- 				le monteze pe faruri obisnuite</p>
+ 				<p  style='color:white;font-size:120%;'>$text16</p>
  
- 				<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+ 				<p  style='text-align:left;font-size:110%;'>$text17</p>
  			</article>
 	</div>
 </div>
-
+";
+?>
 <?php
 include("manage_comments.php");
 include("display_comments.php");
