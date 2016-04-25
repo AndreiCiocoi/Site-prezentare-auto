bb7ce6c31a542083091223e472f9801ed32fa213
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-25 18:44:23 +0300

    Update stire8.php

--- stire8.php
+++ stire8.php
@@ -10,94 +10,73 @@
 			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
 		</div>
 	</div>
-	
-	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+<?php	
+$con = mysql_connect('localhost','root','');
+ 
+if (!$con)
+{
+  die('Could not connect: ' . mysql_error());
+}
+ 
+mysql_select_db('auto', $con);
 
-		<img src="imagini/imagine_stire8.jpg">
+$query = mysql_query("SELECT * FROM stiri WHERE id = 8") or die(mysql_error());
+while($row = mysql_fetch_array($query)){
+	$titlu = $row['titlu'];		$text11 = $row['text11'];		
+	$text1 = $row['text1'];		$text12 = $row['text12'];		
+	$text2 = $row['text2'];		$text13 = $row['text13'];		
+	$text3 = $row['text3'];				
+	$text4 = $row['text4'];			
+	$text5 = $row['text5'];				
+	$text6 = $row['text6'];				
+	$text7 = $row['text7'];			
+	$text8 = $row['text8'];				
+	$text9 = $row['text9'];			
+	$text10 = $row['text10'];			
+	
+}
+echo "	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+
+		<img src='imagini/imagine_stire8.jpg'>
 
 		<article>
-			<h1 style='color:#5CABFF;'>Ce trebuie sa faca in Romania posesorii de masini Volkswagen, Skoda, Audi si Seat diesel?</h1>
+			<h1 style='color:#5CABFF;'>$titlu</h1>
 
-			<h3>La nivel mondial, numarul masinilor dotate cu softul mincinos care pacalea masuratorile noxelor a  
- 			ajuns la peste 12 milioane de unitati. Aici vorbim de masini marca Volkswagen, Audi, Seat sau Skoda,   
- 			dotate cu motorizarile TDI de 1.2, 1.6 sau 2.0 litri fabricate intre 2008 si 2015. Evident, o parte dintre  
-			aceste automobile s-au vandut si in Romania.</h3>
+			<h3>$text1</h3>
  
- 			<p style='font-size:120%;'>Mai bine de 2 milioane de masini Audi, peste 1 milion de unitati Skoda, cam tot atatea masini Seat si vreo 8 milioane de 
- 			automobile Volkswagen au fost vandute in toata lumea din 2008 incoace cu motorizarile diesel care sunt suspectate de a avea 
- 			instalat softul mincinos. Asa ca este firesc sa ne trezim si la noi in tara cu mai multe modele care circula nestingherite pe  
- 			strazile noastre, modele ce fac parte din acest lot urias de masini deloc legale. Si asta pentru ca sunt subiectul unui scandal 
-			urias numit DieselGate in care compania Volkswagen a fost initial acuzata ca pacaleste masuratorile oficiale in 
-			Statele Unite, dar ulterior si in Europa.</p>
+ 			<p style='font-size:120%;'>$text2</p>
  
- 			<a target="_blank" href="imagini/imagine_stire8-2.jpg"><img src="imagini/imagine_stire8-2.jpg"></a>
+ 			<a target='_blank' href='$text3'><img src='$text3'></a>
  			
- 			<p style='font-size:120%;'>Este clar ca romanii au fost pana acum destul de orbiti de faima marcii Volkswagen ce se definea odinioara prin cuvinte ca 
- 			fiabilitate, eficienta si valoare. Aceste atribute au devenit istorie de ani buni, dar romanul de rand tot nu a renuntat la 
- 			automobilele germane si a continuat sa le cumpere, in ciuda problemelor avute de diverse modele, in ciuda problemelor de  
-			garantie si in pofida defectelor de fabricatie. Cu toate astea, romanii au continuat sa cumpere masini Volkswagen, Seat, Skoda  
-			si Audi, dovada fiind cifrele destul de bune. Poate ca acest scandal care s-a propagat global le va trage un semnal de alarma 
-			acestor indivizi care stiau una si buna: Volkswagen. Si le va arata adevarata fata a unui brand care nu mai este de foarte multa 
-			vreme ce a fost candva...</p>
+ 			<p style='font-size:120%;'>$text4</p>
  
- 			<p style='font-size:120%;'>Insa aceste cifre bune la vanzare avute pana acum, intre 2008 si 2015, au dus la vanzarea unui numar destul de mare de 
- 			masini care sunt acum incadrate in lotul problematic. Pentru ca scopul in viata romanului de rand este un Volkswagen diesel, 
-			dotat cu motorul de 2.0 TDI. Pentru multi acest motor care se pare ca polueaza de 40 de ori mai mult decat este declarat in  
-			documente, reprezinta chintesenta perfectiunii in domeniul auto, cel mai bun motor din galaxie! Ce-i drept, nu este un 
-			motor rau, este economic si silentios, fiabil si rezistent. Insa cu asemenea valori uriase de noxe eliminate pe 
-			evacuare, oricine alt producator putea sa faca motoare identice... Mai greu este sa faci un motor si bun, dar si 
-			ecologic, care sa aiba noxe sincere, conform legii. Ei bine, Volkswagen se pare ca nu a reusit sa faca asta, asa ca a 
-			preferat sa minta.
+ 			<p style='font-size:120%;'>$text5
 			</p>
  
- 			<a target="_blank" href="imagini/imagine_stire8-3.jpg"><img src="imagini/imagine_stire8-3.jpg"></a>
+ 			<a target='_blank' href='$text6'><img src='$text6'></a>
 			
-			<p style='font-size:120%;'>Chiar si asa, vorbim de zeci de mii de romani care si-au cumparat in perioada asta masini TDI cu propulsoare de 1.2 litri, 1.6 
- 			litri sau 2.0 litri. Nu conteaza ca acestea au fost montate pe Passat, Polo, Golf, Fabia, Octavia, Leon, Ibiza, A4, A3 sau Superb.  
-			Cert este ca in Romania, la fel ca si in mai toate tarile europene, circula multe masini cu softul mincinos, masini care vor face  
-			obiectul unui recall masiv in service. Asadar, ce trebuie sa faca posesorii de asemenea masini?
+			<p style='font-size:120%;'>$text7
 			</p>
 			
-			<p style='font-size:120%;'>Absolut nimic. Acesta este raspunsul. Dar nici bine nu le va fi... Pe langa pretul care este in scadere, din cauza ca nu se stie ce 
- 			le rezerva viitorul, mai este si posibilitatea reala a unui recall. Noi credem ca Volkswagen nu isi va permite un recall de   
-			peste 12 milioane de unitati la nivel global. Si cand vorbim de recall, ne gandim la rezolvarea problemei initiale. Nu la   
-			dezinstalarea softului ci la modificarea motorului astfel incat sa poata scoate noxele declarate si inregistrate.
-			Interventie ce presupune costuri colosale, pentru ca vorbim de instalarea unor filtre de particule mult mai performante si de 
-			modificarea softului electronic astfel incat masina nu va mai avea aceleasi performante. Dar oare vor fi de acord proprietarii 
-			masinilor cu asta?</p>
+			<p style='font-size:120%;'>$text8</p>
 			
-			<a target="_blank" href="imagini/imagine_stire8-4.jpg"><img src="imagini/imagine_stire8-4.jpg"></a>
+			<a target='_blank' href='$text9'><img src='$text9'></a>
 			
-			<p style='font-size:120%;'>Desi este putin probabil un recall atat de masiv la nivel mondial, pentru ca vorbim de costuri imense care ar falimenta 
- 			compania fara indoiala, aceasta este singura optiune fezabila. Daca masinile nu vor fi modificate, autoritatile americane si    
-			germane care fac masuratorile oficiale vor fi nevoite sa schimbe datele astfel incat masinile nu vor mai corespunde cu limitele    
-			legale. Ceea ce se traduce prin taxe mai mari, pret mai mic la revanzare si inapoierea subventiilor acolo unde este cazul.
+			<p style='font-size:120%;'> $text10
 			</p>
 			
-			<p style='font-size:120%;'>Sa luam cazul Romaniei. Autoritatile germane spun ca un motor 2.0 TDI care pana acum ne-a mintit ca este conform  
- 			normei de poluare Euro 5, este de fapt Euro 3. Acestia transmit informatia catre Registrul Auto Roman care modifica     
-			baza de date si spune ca anume automobile Volkswagen, Audi, Seat sau Skoda care ar trebui sa fie Euro 5 sunt acum    
-			Euro 3. Ceea ce inseamna ca brusc, valoarea acelor masini scade si in cazul schimbarii de proprietar va fi nevoie de o 
-			taxa de poluare mai mare de platit. Sa zicem ca Volkswagen face rechemarea in service. Omul se poate duce pentru 
-			modificare sau nu se poate duce ca nu il obliga nimeni. Daca Volkswagen face interventia si readuce masina la norma 
-			Euro 5, totul este in regula. Daca nu face rechemarea, inseamna ca masinile raman Euro 3 (sau Euro 4, 2, 1 sau chiar 
-			non-euro, cine stie?) si proprietarii sunt buni de plata. Proprietari care, la randul lor, vor da in judecata compania 
-			Volkswagen...
+			<p style='font-size:120%;'>$text11
 			</p>
 			
-			<p style='font-size:120%;'>Concluzia? Mingea este in totalitate in terenul celor de la Volkswagen care acum asteapta niste rapoarte interne si niste 
-			masuratori oficiale guvernamentale pentru a vedea care este urmatorul pas. Cei de la VW nu au alta sansa decat sa modifice 
-			automobilele cu probleme, toate cele 12 milioane de unitati. Daca vor face asta, au toate sansele sa falimenteze din cauza 
-			costurilor uriase. Daca nu fac asta, autoritatile vor schimba datele mincinoase cu unele reale si automobilele nu vor fi mai 
-			respecta legea, iar posesorii vor fi buni de plata si vor da in judecata la randul lor producatorul. Adica oricum, Volkswagen 
-			plateste. Cum vor alege sa abordeze situatia, doar ei stiu...
+			<p style='font-size:120%;'>$text12
 			</p>
  
- 			<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+ 			<p  style='text-align:left;font-size:110%;'>$text13</p>
 		</article>
 	</div>
 </div>
-
+";
+?>
 <?php
 include("manage_comments.php");
 include("display_comments.php");
