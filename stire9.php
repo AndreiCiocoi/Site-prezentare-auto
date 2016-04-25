d2591e555cedaade6cd146ede999ebfdb89dbb5e
sebyschlachter <seby_schlachter@yahoo.com>
2016-04-25 18:56:35 +0300

    Update stire9.php

--- stire9.php
+++ stire9.php
@@ -10,53 +10,65 @@
 			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
 		</div>
 	</div>
-	
-	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+<?php	
+$con = mysql_connect('localhost','root','');
+ 
+if (!$con)
+{
+  die('Could not connect: ' . mysql_error());
+}
+ 
+mysql_select_db('auto', $con);
 
-		<img src="imagini/imagine_stire9.jpg">
+$query = mysql_query("SELECT * FROM stiri WHERE id = 9") or die(mysql_error());
+while($row = mysql_fetch_array($query)){
+	$titlu = $row['titlu'];		$text11 = $row['text11'];		
+	$text1 = $row['text1'];		
+	$text2 = $row['text2'];			
+	$text3 = $row['text3'];				
+	$text4 = $row['text4'];			
+	$text5 = $row['text5'];				
+	$text6 = $row['text6'];				
+	$text7 = $row['text7'];			
+	$text8 = $row['text8'];				
+	$text9 = $row['text9'];			
+	$text10 = $row['text10'];			
+	
+}
+echo "	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+
+		<img src='imagini/imagine_stire9.jpg'>
 
 		<article>
-			<h1 style='color:#5CABFF;'>Porsche mai strecoara in gama un model in patru cilindri. Cu ce valori se lauda noul 718 Cayman</h1>
+			<h1 style='color:#5CABFF;'>$titlu</h1>
 
-			<h3>Porsche prezinta cu ocazia Salonului Auto ce se desfasoara zilele acestea la Beijing versiunea   
- 			imbunatatita a gamei Cayman, disponibila exclusiv in compania unor motorizari supraalimentate cu    
- 			patru cilindri si capacitati de maximum 2.5 litri.</h3>
+			<h3>$text1</h3>
  
- 			<p style='font-size:120%;'>Audi, BMW si Mercedes nu sunt singurele companii auto germane care au venit cu motoare mai mici, 
-			la nivel de capacitate si / sau numar de cilindri, pentru modelele lor de inalta performanta.</p>
+ 			<p style='font-size:120%;'>$text2</p>
  
- 			<a target="_blank" href="imagini/imagine_stire9-2.jpg"><img src="imagini/imagine_stire9-2.jpg"></a>
+ 			<a target='_blank' href='$text3'><img src='$text3'></a>
  			
- 			<p style='font-size:120%;'>Pe un trend similar au mers si rivalii de la Porsche, care au prezentat la inceputul acestui an versiunea imbunatatita a gamei 
- 			Boxster - singura disponibila exclusiv alaturi de motorizari supraalimentate cu patru cilindri. 
- 			Singura pana acum, de un tratament similar avand parte si modelul Cayman, care preia de la roadster-ul anterior mentionat   
-			atat indicativul 718, cat si cele doua propulsoare turbo.Intr-o alta ordine de idei, fanii automobilului cu motor central din Stuttgart vor avea acum la 
-			dispozitie fie un 2.0 de 300 CP, fie un 2.5 de 350 CP. Fisa tehnica este completate in ambele cazuri de doua tipuri de transmisie.</p>
+ 			<p style='font-size:120%;'> $text4</p>
  
- 			<p style='font-size:120%;'>Cei care prefera sa ramana la traditionala cutie manuala in sase rapoarte vor experimenta un timp de 5.1, respectiv 4.6 
-			secunde pentru acceleratia de la 0 la 100 kilometri pe ora.Opteaza pentru unitatea PDK cu dublu ambreiaj si pachetul Sport Chrono, iar aceeasi ecuatie 
-			isi va gasi rezolvarea dupa numai 4.7 sau 4.2 secunde - in functie de motorizarea pentru care ai optat anterior.
+ 			<p style='font-size:120%;'>$text5
 			</p>
  
- 			<a target="_blank" href="imagini/imagine_stire9-3.jpg"><img src="imagini/imagine_stire9-3.jpg"></a>
+ 			<a target='_blank' href='$text6'><img src='$text6'></a>
 			
-			<p style='font-size:120%;'>Valoarea din dreptul vitezei maxime nu tine cont de tipul transmisiei alese, ci mai degraba de numarul cailor putere. Asadar, 
- 			fisa tehnica anunta un 275 km/h pentru versiunea Porsche 718 Cayman si un 285 km/h pentru varianta Porsche 718 
-			Cayman S.</p>
+			<p style='font-size:120%;'>$text7</p>
 			
-			<p style='font-size:120%;'>Din punct de vedere estetic, atat modificarile exterioare, cat si cele interioare propuse de automobilul cu motor central din 
- 			Stuttgart sunt similare celor aduse de modelul Boxster.</p>
+			<p style='font-size:120%;'>$text8</p>
 			
-			<a target="_blank" href="imagini/imagine_stire9-4.jpg"><img src="imagini/imagine_stire9-4.jpg"></a>
+			<a target='_blank' href='$text9'><img src='$text9'></a>
 			
-			<p style='font-size:120%;'>Noua gama Porsche 718 Cayman este deja disponibila la vanzare, preturile pentru Germania pornind de la 51.623 
- 			euro pentru versiunea de baza 718 Cayman si 64.118 euro pentru varianta de top 718 Cayman S.</p>
+			<p style='font-size:120%;'>$text10</p>
  
- 			<p id="sursa" style='text-align:left;font-size:110%;'><b>Sursa:www.4tuning.ro</b></p>
+ 			<p  style='text-align:left;font-size:110%;'>$text11</p>
 		</article>
 	</div>
 </div>
-
+";
+?>
 <?php
 include("manage_comments.php");
 include("display_comments.php");
