f059a387d5162bd9cfb87efcf778a9a376bcab48
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-25 12:38:27 +0300

    Create stire8.php

---
+++ stire8.php
@@ -0,0 +1,59 @@
+<div style='background-color:black;'>
+ 	
+ 	<div id="header" style='width: 300px;height: 10px;margin: 0px auto;'>
+
+		<div id="logo"style='float:left;'>	
+			<a href="index.html" style='color:#FFFFFF;text-decoration: none;'><h1>BestCars</h1></a>
+		</div>
+		
+		<div id="logocar" style='float:right;'>	
+			<a href="index.html"><img src="imagini/Logo 100X63.png"></a>
+		</div>
+	</div>
+	
+	<div style='text-align:center;color:#FFFFFF;width: 950px;margin: 0px auto;'>
+
+		<img src="imagini/imagine_stire8.jpg">
+
+		<article>
+			<h1 style='color:#5CABFF;'>Ce trebuie sa faca in Romania posesorii de masini Volkswagen, Skoda, Audi si Seat diesel?</h1>
+
+			<h3>La nivel mondial, numarul masinilor dotate cu softul mincinos care pacalea masuratorile noxelor a  
+ 			ajuns la peste 12 milioane de unitati. Aici vorbim de masini marca Volkswagen, Audi, Seat sau Skoda,   
+ 			dotate cu motorizarile TDI de 1.2, 1.6 sau 2.0 litri fabricate intre 2008 si 2015. Evident, o parte dintre  
+			aceste automobile s-au vandut si in Romania.</h3>
+ 
+ 			<p style='font-size:120%;'>Mai bine de 2 milioane de masini Audi, peste 1 milion de unitati Skoda, cam tot atatea masini Seat si vreo 8 milioane de 
+ 			automobile Volkswagen au fost vandute in toata lumea din 2008 incoace cu motorizarile diesel care sunt suspectate de a avea 
+ 			instalat softul mincinos. Asa ca este firesc sa ne trezim si la noi in tara cu mai multe modele care circula nestingherite pe  
+ 			strazile noastre, modele ce fac parte din acest lot urias de masini deloc legale. Si asta pentru ca sunt subiectul unui scandal 
+			urias numit DieselGate in care compania Volkswagen a fost initial acuzata ca pacaleste masuratorile oficiale in 
+			Statele Unite, dar ulterior si in Europa.</p>
+ 
+ 			<a target="_blank" href="imagini/imagine_stire8-2.jpg"><img src="imagini/imagine_stire8-2.jpg"></a>
+ 			
+ 			<p style='font-size:120%;'>Este clar ca romanii au fost pana acum destul de orbiti de faima marcii Volkswagen ce se definea odinioara prin cuvinte ca 
+ 			fiabilitate, eficienta si valoare. Aceste atribute au devenit istorie de ani buni, dar romanul de rand tot nu a renuntat la 
+ 			automobilele germane si a continuat sa le cumpere, in ciuda problemelor avute de diverse modele, in ciuda problemelor de  
+			garantie si in pofida defectelor de fabricatie. Cu toate astea, romanii au continuat sa cumpere masini Volkswagen, Seat, Skoda  
+			si Audi, dovada fiind cifrele destul de bune. Poate ca acest scandal care s-a propagat global le va trage un semnal de alarma 
+			acestor indivizi care stiau una si buna: Volkswagen. Si le va arata adevarata fata a unui brand care nu mai este de foarte multa 
+			vreme ce a fost candva...</p>
+ 
+ 			<p style='font-size:120%;'>Insa aceste cifre bune la vanzare avute pana acum, intre 2008 si 2015, au dus la vanzarea unui numar destul de mare de 
+ 			masini care sunt acum incadrate in lotul problematic. Pentru ca scopul in viata romanului de rand este un Volkswagen diesel, 
+			dotat cu motorul de 2.0 TDI. Pentru multi acest motor care se pare ca polueaza de 40 de ori mai mult decat este declarat in  
+			documente, reprezinta chintesenta perfectiunii in domeniul auto, cel mai bun motor din galaxie! Ce-i drept, nu este un 
+			motor rau, este economic si silentios, fiabil si rezistent. Insa cu asemenea valori uriase de noxe eliminate pe 
+			evacuare, oricine alt producator putea sa faca motoare identice... Mai greu este sa faci un motor si bun, dar si 
+			ecologic, care sa aiba noxe sincere, conform legii. Ei bine, Volkswagen se pare ca nu a reusit sa faca asta, asa ca a 
+			preferat sa minta.
+			</p>
+ 
+ 			<a target="_blank" href="imagini/imagine_stire7-3.jpg"><img src="imagini/imagine_stire8-3.jpg"></a>
+			
+			<p style='font-size:120%;'>Chiar si asa, vorbim de zeci de mii de romani care si-au cumparat in perioada asta masini TDI cu propulsoare de 1.2 litri, 1.6 
+ 			litri sau 2.0 litri. Nu conteaza ca acestea au fost montate pe Passat, Polo, Golf, Fabia, Octavia, Leon, Ibiza, A4, A3 sau Superb.  
+			Cert este ca in Romania, la fel ca si in mai toate tarile europene, circula multe masini cu softul mincinos, masini care vor face  
+			obiectul unui recall masiv in service. Asadar, ce trebuie sa faca posesorii de asemenea masini?
+			</p>
