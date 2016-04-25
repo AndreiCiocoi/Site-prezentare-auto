830cd4fd9f1f9f3539452dd7d6c54ada414400c2
AndreiCiocoi <andrei.ciocoi@yahoo.com>
2016-04-25 12:39:11 +0300

    Update stire8.php

--- stire8.php
+++ stire8.php
@@ -57,3 +57,38 @@
 			Cert este ca in Romania, la fel ca si in mai toate tarile europene, circula multe masini cu softul mincinos, masini care vor face  
 			obiectul unui recall masiv in service. Asadar, ce trebuie sa faca posesorii de asemenea masini?
 			</p>
+			
+			<p style='font-size:120%;'>Absolut nimic. Acesta este raspunsul. Dar nici bine nu le va fi... Pe langa pretul care este in scadere, din cauza ca nu se stie ce 
+ 			le rezerva viitorul, mai este si posibilitatea reala a unui recall. Noi credem ca Volkswagen nu isi va permite un recall de   
+			peste 12 milioane de unitati la nivel global. Si cand vorbim de recall, ne gandim la rezolvarea problemei initiale. Nu la   
+			dezinstalarea softului ci la modificarea motorului astfel incat sa poata scoate noxele declarate si inregistrate.
+			Interventie ce presupune costuri colosale, pentru ca vorbim de instalarea unor filtre de particule mult mai performante si de 
+			modificarea softului electronic astfel incat masina nu va mai avea aceleasi performante. Dar oare vor fi de acord proprietarii 
+			masinilor cu asta?</p>
+			
+			<a target="_blank" href="imagini/imagine_stire7-3.jpg"><img src="imagini/imagine_stire7-3.jpg"></a>
+			
+			<p style='font-size:120%;'>Desi este putin probabil un recall atat de masiv la nivel mondial, pentru ca vorbim de costuri imense care ar falimenta 
+ 			compania fara indoiala, aceasta este singura optiune fezabila. Daca masinile nu vor fi modificate, autoritatile americane si    
+			germane care fac masuratorile oficiale vor fi nevoite sa schimbe datele astfel incat masinile nu vor mai corespunde cu limitele    
+			legale. Ceea ce se traduce prin taxe mai mari, pret mai mic la revanzare si inapoierea subventiilor acolo unde este cazul.
+			</p>
+			
+			<p style='font-size:120%;'>Sa luam cazul Romaniei. Autoritatile germane spun ca un motor 2.0 TDI care pana acum ne-a mintit ca este conform  
+ 			normei de poluare Euro 5, este de fapt Euro 3. Acestia transmit informatia catre Registrul Auto Roman care modifica     
+			baza de date si spune ca anume automobile Volkswagen, Audi, Seat sau Skoda care ar trebui sa fie Euro 5 sunt acum    
+			Euro 3. Ceea ce inseamna ca brusc, valoarea acelor masini scade si in cazul schimbarii de proprietar va fi nevoie de o 
+			taxa de poluare mai mare de platit. Sa zicem ca Volkswagen face rechemarea in service. Omul se poate duce pentru 
+			modificare sau nu se poate duce ca nu il obliga nimeni. Daca Volkswagen face interventia si readuce masina la norma 
+			Euro 5, totul este in regula. Daca nu face rechemarea, inseamna ca masinile raman Euro 3 (sau Euro 4, 2, 1 sau chiar 
+			non-euro, cine stie?) si proprietarii sunt buni de plata. Proprietari care, la randul lor, vor da in judecata compania 
+			Volkswagen...
+			</p>
+			
+			<p style='font-size:120%;'>Concluzia? Mingea este in totalitate in terenul celor de la Volkswagen care acum asteapta niste rapoarte interne si niste 
+			masuratori oficiale guvernamentale pentru a vedea care este urmatorul pas. Cei de la VW nu au alta sansa decat sa modifice 
+			automobilele cu probleme, toate cele 12 milioane de unitati. Daca vor face asta, au toate sansele sa falimenteze din cauza 
+			costurilor uriase. Daca nu fac asta, autoritatile vor schimba datele mincinoase cu unele reale si automobilele nu vor fi mai 
+			respecta legea, iar posesorii vor fi buni de plata si vor da in judecata la randul lor producatorul. Adica oricum, Volkswagen 
+			plateste. Cum vor alege sa abordeze situatia, doar ei stiu...
+			</p>
